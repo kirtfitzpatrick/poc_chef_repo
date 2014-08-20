45d081b4fa069259e3be294d5a8def5fe5c548bc
Kirt Fitzpatrick <kirt.fitzpatrick@autodesk.com>
2014-08-20 14:35:52 -0700

    removed the job.sh

--- job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh 
-PATH=/opt/chef/embedded/bin:/sbin:/usr/sbin:/bin:/usr/bin
-export PATH=/opt/chef/embedded/bin:/sbin:/usr/sbin:/bin:/usr/bin
-
-for env in `ls -1 environments`;
-do
-  berks apply $env --lockfile=environments/$env/Berksfile.lock
-done
